3aa802ddb93f9a2ffc4606c753dd5b474203f452
rajatnparth <rajatnparth@gmail.com>
2023-03-18 08:59:15 +0530

    Add files via upload
    Uploading Spring Boot Rest API backend of url-shortener

---
+++ url-shortener/src/main/java/com/yebelo/urlshortener/entity/UrlDetails.java
@@ -0,0 +1,48 @@
+package com.yebelo.urlshortener.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class UrlDetails {
+	
+	@Id
+	@GeneratedValue
+	Long id;
+	String url;
+	String shortUrl;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public String getShortUrl() {
+		return shortUrl;
+	}
+	public void setShortUrl(String shortUrl) {
+		this.shortUrl = shortUrl;
+	}
+	public UrlDetails() {
+		
+	}
+	public UrlDetails(Long id, String url, String shortUrl) {
+		super();
+		this.id = id;
+		this.url = url;
+	}
+	@Override
+	public String toString() {
+		return "UrlDetails [id=" + id + ", url=" + url + ", shortUrl=" + shortUrl + "]";
+	}
+	
+
+}
